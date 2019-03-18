56602b906d8d1a4fa764028ed434dc974040fd1d
Lukas Körfer <lu.koerfer@googlemail.com>
2019-03-18 23:52:53 +0100

    Renamed project, introduced concept of package setups

--- annotations/src/main/java/de/lukaskoerfer/autoplementations/annotations/Autoplementations.java
+++ annotations/src/main/java/de/lukaskoerfer/autoplementations/annotations/Autoplementations.java
@@ -10,7 +10,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.TYPE)
-public @interface Implementations {
+public @interface Autoplementations {
 	
 	Autoplementation[] value() default {};
 	
